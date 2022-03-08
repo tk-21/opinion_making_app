8507d31110afef25f41e5f167ffdb2d0919308b2
tk <83660733+tk-21@users.noreply.github.com>
2022-03-08 23:55:31 +0900

    ログイン画面のスタイル調整

--- app/views/login.php
+++ app/views/login.php
@@ -9,33 +9,31 @@ function index()
     \partials\header();
 ?>
 
-                <section class="login">
-                    <div class="auth-inner">
-                        <form class="form validate-form" action="" method="POST" novalidate>
-                            <h2 class="form-ttl">ログイン</h2>
-                            <dl class="form-list">
-                                <dt class="form-dttl"><label for="name" onclick="">ユーザーネーム</label></dt>
-                                <dd class="form-item">
-                                    <input type="text" name="name" class="form-text form-control validate-target" autofocus required minlength="4" maxlength="10" pattern="[a-zA-Z0-9]+">
-                                    <div class="invalid-feedback"></div>
-                                </dd>
-                            </dl>
-                            <dl class="form-list">
-                                <dt class="form-dttl"><label for="password" onclick="">パスワード</label></dt>
-                                <dd class="form-item">
-                                    <input type="password" name="password" class="form-text form-control validate-target" autofocus required minlength="4" maxlength="10" pattern="[a-zA-Z0-9]+">
-                                    <div class="invalid-feedback"></div>
-                                </dd>
-                            </dl>
-                            <button type="submit" class="submit-btn">ログイン</button>
-                            <div>
-                                <a href="<?php the_url('register'); ?>">アカウント登録</a>
-                            </div>
-                        </form>
-                    </div>
-                </section>
+    <section class="login">
+        <div class="auth-inner">
+            <form class="form validate-form" action="<?php echo CURRENT_URI; ?>" method="POST" novalidate>
+                <h2 class="form-ttl">ログイン</h2>
+                <dl class="form-list">
+                    <dt class="form-dttl"><label for="name" onclick="">ユーザーネーム</label></dt>
+                    <dd class="form-item">
+                        <input type="text" name="name" class="form-text form-control validate-target" autofocus required minlength="4" maxlength="10" pattern="[a-zA-Z0-9]+">
+                        <div class="invalid-feedback"></div>
+                    </dd>
+                </dl>
+                <dl class="form-list">
+                    <dt class="form-dttl"><label for="password" onclick="">パスワード</label></dt>
+                    <dd class="form-item">
+                        <input type="password" name="password" class="form-text form-control validate-target" autofocus required minlength="4" maxlength="10" pattern="[a-zA-Z0-9]+">
+                        <div class="invalid-feedback"></div>
+                    </dd>
+                </dl>
+                <button type="submit" class="submit-btn">ログイン</button>
+            </form>
+            <p class="auth-txt"><a href="<?php the_url('register'); ?>">アカウント登録</a></p>
+        </div>
+    </section>
 
-            <?php
-            \partials\footer();
-        }
-            ?>
+<?php
+    \partials\footer();
+}
+?>
