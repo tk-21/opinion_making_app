e3613d40527de7fa0ddb3f7983abf476db9a560e
tk <83660733+tk-21@users.noreply.github.com>
2022-03-09 22:44:12 +0900

    メッセージ表示のcss調整

--- app/libs/message.php
+++ app/libs/message.php
@@ -51,7 +51,6 @@ class Msg extends AbstractModel
             // もしセッションから何もとれてこなかったら空の配列を代入
             $msgs_with_type = static::getSessionAndFlush() ?? [];
 
-            echo '<div id="messages">';
             foreach ($msgs_with_type as $type => $msgs) {
                 // $typeにデバッグが回ってきたとき、デバッグフラグがfalse（本番環境）だったら次のループにステップする
                 if ($type === static::DEBUG && !DEBUG) {
@@ -59,14 +58,12 @@ class Msg extends AbstractModel
                 }
 
                 // メッセージのタイプによって色を変える処理
-                $color = $type === static::INFO ? 'alert-info' : 'alert-danger';
+                $color = $type === static::INFO ? 'msg-info' : 'msg-error';
 
                 foreach ($msgs as $msg) {
-                    echo "<div class='alert $color'>{$msg}</div>";
+                    echo "<p class='$color'>{$msg}</p>";
                 }
             }
-
-            echo '</div>';
         } catch (Throwable $e) {
             Msg::push(Msg::DEBUG, $e->getMessage());
             // メッセージを表示させて改修がしやすいようにしておく
