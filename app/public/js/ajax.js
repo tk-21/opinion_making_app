d97e5d1c4b224b3a9e3c10f77cfa6159450d4499
tk <83660733+tk-21@users.noreply.github.com>
2022-08-17 23:43:13 +0900

    ajaxを一部修正

--- app/public/js/ajax.js
+++ app/public/js/ajax.js
@@ -15,89 +15,88 @@
 // });
 
 $("#objection-register").on("click", function () {
-    var uri = new URL(window.location.href);
-    var url = uri.origin;
-
-    var data = {
+    let data = {
         topic_id: $("#topic_id").val(),
         form_type: $("#form_type").val(),
         objection: $("#objection").val(),
     };
 
     $.ajax({
-        url: url + "/detail",
+        url: window.location.href,
         type: "post",
         data: data,
     }).then(
-        //成功したとき
+        //     //成功したとき
         function (data) {
             // alert("成功");
-            //     let json = JSON.parse(data); //オブジェクト化
-            //     console.log("success", json);
-            //     if (json.result == "success") {
-            //         //index.phpに遷移させる
-            // window.location.href = url + "detail?id=" + topic_id;
-            //     } else {
-            //         //削除に失敗
-            //         console.log("failed to delete");
-            //         alert("failed to delete.");
-            //         //削除ボタン活性化
-            //         $(".delete-btn").prop("disabled", false);
-            //     }
-        },
-        //失敗したとき
-        function () {
-            console.log("fail");
-            alert("fail");
-            //削除ボタン活性化
-            $(".delete-btn").prop("disabled", false);
+            //         // let json = JSON.parse(data); //オブジェクト化
+            //         // console.log("success", json);
+            //         //     if (json.result == "success") {
+            //         //         //index.phpに遷移させる
+            // let uri = new URL(window.location.href);
+            // let url = uri.href;
+            // window.location.href = uri.href;
+            //         //     } else {
+            //         //         //削除に失敗
+            //         //         console.log("failed to delete");
+            //         //         alert("failed to delete.");
+            //         //         //削除ボタン活性化
+            //         //         $(".delete-btn").prop("disabled", false);
+            //         //     }
         }
+        //     //失敗したとき
+        //     function () {
+        //         console.log("fail");
+        //         alert("fail");
+        //         //削除ボタン活性化
+        //         $(".delete-btn").prop("disabled", false);
+        //     }
     );
 });
+// });
+// $(".todo-checkbox").change(function () {
+//     let todo_id = $(this).data("id");
+//     //data配列にtodo_idを渡す
 
-$(".todo-checkbox").change(function () {
-    let todo_id = $(this).data("id");
-    //data配列にtodo_idを渡す
+//     let data = {};
+//     data.todo_id = todo_id;
 
-    let data = {};
-    data.todo_id = todo_id;
+//     $.ajax({
+//         url: "./update_status.php", //通信先
+//         type: "post", //リクエストメソッド
+//         data: data, //データを渡して通信
+//     }).then(
+//         //ajax通信に成功したとき
+//         function (data) {
+//             let json = JSON.parse(data); //文字列データをjson化する
+//             console.log("success", json);
+//             //jsonのresultプロパティがsuccessだったら、
+//             if (json.result == "success") {
+//                 console.log("success");
 
-    $.ajax({
-        url: "./update_status.php", //通信先
-        type: "post", //リクエストメソッド
-        data: data, //データを渡して通信
-    }).then(
-        //ajax通信に成功したとき
-        function (data) {
-            let json = JSON.parse(data); //文字列データをjson化する
-            console.log("success", json);
-            //jsonのresultプロパティがsuccessだったら、
-            if (json.result == "success") {
-                console.log("success");
+//                 //.todo-checkboxクラスから見て2つ上の親要素の中にある.statusクラスのテキストを取得
+//                 let text = $(this).parent().parent().find(".status").text();
+//                 console.log(text);
 
-                //.todo-checkboxクラスから見て2つ上の親要素の中にある.statusクラスのテキストを取得
-                let text = $(this).parent().parent().find(".status").text();
-                console.log(text);
+//                 //入れ替え
+//                 if (text == "完了") {
+//                     text = "未完了";
+//                 } else if (text == "未完了") {
+//                     text = "完了";
+//                 }
 
-                //入れ替え
-                if (text == "完了") {
-                    text = "未完了";
-                } else if (text == "未完了") {
-                    text = "完了";
-                }
-
-                //入れ替えたものを設定
-                $(this).parent().parent().find(".status").text(text);
-            } else {
-                //削除に失敗
-                console.log("failed to update status");
-                alert("failed to update status.");
-            }
-        }.bind(this), //ajax通信の中でthisがうまく取得できないときはbind(this)が記述する
-        //ajax通信に失敗したとき
-        function () {
-            console.log("fail");
-            alert("failed to ajax");
-        }
-    );
-});
+//                 //入れ替えたものを設定
+//                 $(this).parent().parent().find(".status").text(text);
+//             } else {
+//                 //削除に失敗
+//                 console.log("failed to update status");
+//                 alert("failed to update status.");
+//             }
+//         }.bind(this), //ajax通信の中でthisがうまく取得できないときはbind(this)が記述する
+//         //ajax通信に失敗したとき
+//         function () {
+//             console.log("fail");
+//             alert("failed to ajax");
+//         }
+//     );
+// });
